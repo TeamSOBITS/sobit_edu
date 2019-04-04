ff3c7976441eb0af4766955679811d70f698b1d6
eierapfel <eierapfel.125@gmail.com>
2019-04-04 17:11:10 +0900

    fix: coding_style

--- sobit_education_control/include/sobit_education_control/sobit_education_hw.h
+++ sobit_education_control/include/sobit_education_control/sobit_education_hw.h
@@ -1,49 +1,46 @@
 #ifndef SOBIT_EDUCATION_HW_H_
 #define SOBIT_EDUCATION_HW_H_
 
-#include <ros/ros.h>
 #include <ros/package.h>
-#include "sobit_common_dynamixel.h"
 #include <ros/ros.h>
-#include <trajectory_msgs/JointTrajectory.h>
 #include <sensor_msgs/JointState.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include "sobit_common_dynamixel.h"
 
-class SobitEducationDynamixel: public SobitCommonDynamixel
-{ 
-public:
+class SobitEducationDynamixel : public SobitCommonDynamixel {
+ public:
   SobitEducationDynamixel();
   ~SobitEducationDynamixel();
-  void initializeDynamixel();
-  void writeDynamixelMotors(const trajectory_msgs::JointTrajectory &pose);
+  void                    initializeDynamixel();
+  void                    writeDynamixelMotors(const trajectory_msgs::JointTrajectory &pose);
   sensor_msgs::JointState readDynamixelMotors();
 
-private:
-  int getJointNumber(std::string joint_name);
+ private:
+  int   getJointNumber(std::string joint_name);
   float toBit(int id, float rad);
   float toRad(std::string joint_name, int bit);
 };
 
-class SobitEducationControl : public SobitEducationDynamixel
-{
-public:
+class SobitEducationControl : public SobitEducationDynamixel {
+ public:
   ros::NodeHandle nh_;
 
-protected:
+ protected:
   hardware_interface::JointStateInterface                  jnt_state_interface_;
   hardware_interface::PositionJointInterface               jnt_pos_interface_;
   joint_limits_interface::PositionJointSoftLimitsInterface jnt_limit_interface_;
-  double cmd_[6];
-  double pos_[6];
-  double vel_[6];
-  double eff_[6];
+  double                                                   cmd_[6];
+  double                                                   pos_[6];
+  double                                                   vel_[6];
+  double                                                   eff_[6];
 
-public:
+ public:
   SobitEducationControl();
-  ros::Time getTime() const { return ros::Time::now(); }
+  ros::Time     getTime() const { return ros::Time::now(); }
   ros::Duration getPeriod() const { return ros::Duration(0.01); }
-  void writeInitialJoint();
-  void read(ros::Time, ros::Duration);
-  void write(ros::Time, ros::Duration);
+  void          writeInitialJoint();
+  void          read(ros::Time, ros::Duration);
+  void          write(ros::Time, ros::Duration);
 };
 
 #endif /* SOBIT_EDUCATION_HW_H_ */
--- sobit_education_control/src/sobit_education_dynamixel.cpp
+++ sobit_education_control/src/sobit_education_dynamixel.cpp
@@ -1,23 +1,14 @@
 #include <sobit_education_control/sobit_education_hw.h>
 
-SobitEducationDynamixel::SobitEducationDynamixel()
-{
-  /*
-  dxl_goal_position[1] = toBit(1, 0);
-  dxl_goal_position[16] = toBit(16, 0);
-  dxl_goal_position[17] = toBit(17, 0);
-  dxl_goal_position[18] = toBit(18, 1.31);
-  dxl_goal_position[19] = toBit(19, 0);
-  dxl_goal_position[20] = toBit(20, 0);*/
-
-  used_dynamixel_id[1] = 1;
+SobitEducationDynamixel::SobitEducationDynamixel() {
+  used_dynamixel_id[1]  = 1;
   used_dynamixel_id[16] = 16;
   used_dynamixel_id[17] = 17;
   used_dynamixel_id[18] = 18;
   used_dynamixel_id[19] = 19;
   used_dynamixel_id[20] = 20;
 
-  used_dynamixel_name[1] = "xtion_tilt_joint";
+  used_dynamixel_name[1]  = "xtion_tilt_joint";
   used_dynamixel_name[16] = "arm_roll_joint";
   used_dynamixel_name[17] = "arm_flex_joint";
   used_dynamixel_name[18] = "elbow_flex_joint";
@@ -27,83 +18,87 @@ SobitEducationDynamixel::SobitEducationDynamixel()
 
 SobitEducationDynamixel::~SobitEducationDynamixel() {}
 
-void SobitEducationDynamixel::initializeDynamixel()
-{
-  for (int i = 0; i < 30; i++)
-  {
-    if (used_dynamixel_id[i])
-    {
+void SobitEducationDynamixel::initializeDynamixel() {
+  for (int i = 0; i < 30; i++) {
+    if (used_dynamixel_id[i]) {
       setTorqueEnable(i);
       setAcceleration(i, acceleration_val);
       setVelocity(i, velocity_val);
       setGrouopRead(i);
-      if (i == 20)
-        setTorqueLimit(i);
+      if (i == 20) setTorqueLimit(i);
     }
   }
 }
 
-void SobitEducationDynamixel::writeDynamixelMotors(const trajectory_msgs::JointTrajectory &pose)
-{
-  for(int i = 0; i < pose.joint_names.size(); i++)
-  {
-    int joint_id  = getJointNumber(pose.joint_names[i]);
-    if (!joint_id) {  continue; }
+void SobitEducationDynamixel::writeDynamixelMotors(const trajectory_msgs::JointTrajectory &pose) {
+  for (int i = 0; i < pose.joint_names.size(); i++) {
+    int joint_id = getJointNumber(pose.joint_names[i]);
+    if (!joint_id) {
+      continue;
+    }
     int joint_pos = toBit(joint_id, pose.points[0].positions[i]);
-    if (std::abs(joint_pos == saved_dxl_goal_position[joint_id])) { continue; }
+    if (std::abs(joint_pos == saved_dxl_goal_position[joint_id])) {
+      continue;
+    }
     addPositionToStorage(joint_id, joint_pos);
     saved_dxl_goal_position[joint_id] = joint_pos;
   }
   writeGoalPositon();
 }
 
-sensor_msgs::JointState SobitEducationDynamixel::readDynamixelMotors()
-{
+sensor_msgs::JointState SobitEducationDynamixel::readDynamixelMotors() {
   sensor_msgs::JointState pose;
   dxl_comm_result = readPositonGroup.txRxPacket();
-  for (int i = 0; i < 30; i++)
-  {
-    if (used_dynamixel_id[i])
-    {
-      std::string joint_name = used_dynamixel_name[i];
-      int dynamixel_pos = readCurrentPosition(used_dynamixel_id[i]);
-      float joint_pos = toRad(joint_name, dynamixel_pos);
+  for (int i = 0; i < 30; i++) {
+    if (used_dynamixel_id[i]) {
+      std::string joint_name    = used_dynamixel_name[i];
+      int         dynamixel_pos = readCurrentPosition(used_dynamixel_id[i]);
+      float       joint_pos     = toRad(joint_name, dynamixel_pos);
       pose.name.push_back(joint_name);
       pose.position.push_back(joint_pos);
     }
   }
   return pose;
 }
-  
-int SobitEducationDynamixel::getJointNumber(std::string joint_name)
-{
-  for (int i = 0; i < 30; i++)
-  {
-    if (used_dynamixel_name[i] == joint_name)
-    {
+
+int SobitEducationDynamixel::getJointNumber(std::string joint_name) {
+  for (int i = 0; i < 30; i++) {
+    if (used_dynamixel_name[i] == joint_name) {
       return used_dynamixel_id[i];
     }
   }
   return 0;
 }
 
-float SobitEducationDynamixel::toBit(int id, float rad)
-{
-  if      (id == 16)  { return MAGNIFICATION_VALUE * (rad + M_PI);  }
-  else if (id == 17)  { return MAGNIFICATION_VALUE * (-rad + M_PI); }
-  else if (id == 18)  { return MAGNIFICATION_VALUE * (rad + M_PI);  }
-  else if (id == 19)  { return MAGNIFICATION_VALUE * (rad + M_PI);  }
-  else if (id == 20)  { return 2080 - rad * ( 910 / (M_PI_2) ); }
-  else if (id == 1)   { return MAGNIFICATION_VALUE * (rad + M_PI); }
+float SobitEducationDynamixel::toBit(int id, float rad) {
+  if (id == 16) {
+    return MAGNIFICATION_VALUE * (rad + M_PI);
+  } else if (id == 17) {
+    return MAGNIFICATION_VALUE * (-rad + M_PI);
+  } else if (id == 18) {
+    return MAGNIFICATION_VALUE * (rad + M_PI);
+  } else if (id == 19) {
+    return MAGNIFICATION_VALUE * (rad + M_PI);
+  } else if (id == 20) {
+    return 2080 - rad * (910 / (M_PI_2));
+  } else if (id == 1) {
+    return MAGNIFICATION_VALUE * (rad + M_PI);
+  }
   return -1;
 }
 
-float SobitEducationDynamixel::toRad(std::string joint_name, int bit)
-{
-  if (joint_name == "arm_roll_joint")         { return ( bit / MAGNIFICATION_VALUE) - M_PI;  }
-  else if (joint_name == "arm_flex_joint")    { return - ( bit / MAGNIFICATION_VALUE) + M_PI; }
-  else if (joint_name == "elbow_flex_joint")  { return ( bit / MAGNIFICATION_VALUE) - M_PI; }
-  else if (joint_name == "wrist_flex_joint")  { return ( bit / MAGNIFICATION_VALUE) - M_PI; }
-  else if (joint_name == "hand_motor_joint")  { return - ( bit - 2080 ) / ( 910 / (M_PI_2) );  }
-  else if (joint_name == "xtion_tilt_joint")  { return ( bit / MAGNIFICATION_VALUE) - M_PI;  }
+float SobitEducationDynamixel::toRad(std::string joint_name, int bit) {
+  if (joint_name == "arm_roll_joint") {
+    return (bit / MAGNIFICATION_VALUE) - M_PI;
+  } else if (joint_name == "arm_flex_joint") {
+    return -(bit / MAGNIFICATION_VALUE) + M_PI;
+  } else if (joint_name == "elbow_flex_joint") {
+    return (bit / MAGNIFICATION_VALUE) - M_PI;
+  } else if (joint_name == "wrist_flex_joint") {
+    return (bit / MAGNIFICATION_VALUE) - M_PI;
+  } else if (joint_name == "hand_motor_joint") {
+    return -(bit - 2080) / (910 / (M_PI_2));
+  } else if (joint_name == "xtion_tilt_joint") {
+    return (bit / MAGNIFICATION_VALUE) - M_PI;
+  }
 }
