190a1b31b31137b7a0f689308b6dc536455c9ae5
Yuki-Ikeda0810 <yukiikeda2@gmail.com>
2021-11-10 11:22:55 +0000

    Update sobit_education_library/example/test_wheel.py

--- sobit_education_library/example/test_wheel.py
+++ sobit_education_library/example/test_wheel.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-#coding: utf-8
-
 import rospy
 from sobit_education_module import SobitTurtlebotController
 import sys
@@ -11,14 +9,11 @@ def test():
     edu_wheel_ctr = SobitTurtlebotController(args[0]) # args[0] : C++上でros::init()を行うための引数
     
     # タイヤ車輪をを動かす
-    # edu_wheel_ctr.controlWheelLinear(0.5)
-    
+    edu_wheel_ctr.controlWheelLinear(1.0)
     edu_wheel_ctr.controlWheelRotateRad(1.57)
-    rospy.sleep(3.0)
     edu_wheel_ctr.controlWheelRotateDeg(-90)
-    rospy.sleep(3.0)
 
-    # edu_wheel_ctr.controlWheelLinear(-0.5)
+    edu_wheel_ctr.controlWheelLinear(-1.0)
 
 if __name__ == '__main__':
     try:
