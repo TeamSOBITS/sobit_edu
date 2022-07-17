7e9efd5ae182c799643b8f803239a54ba643ffba
MrKeith99 <kvalentincardenas@hotmail.com>
2022-07-17 20:41:13 +0900

    update pybind with naming

--- sobit_education_library/src/sobit_education_pybind.cpp
+++ sobit_education_library/src/sobit_education_pybind.cpp
@@ -3,7 +3,7 @@
 using namespace sobit_education;
 namespace py = pybind11;
 
-PYBIND11_MODULE(sobit_education_module, m) {
+PYBIND11_MODULE( sobit_education_module, m ) {
     py::enum_<Joint>( m, "Joint" )
         .value( "ARM_ROLL_JOINT", Joint::ARM_ROLL_JOINT )
         .value( "ARM_FLEX_JOINT_RGT", Joint::ARM_FLEX_JOINT_RGT )
@@ -15,22 +15,47 @@ PYBIND11_MODULE(sobit_education_module, m) {
         .value( "XTION_PAN_JOINT", Joint:: XTION_PAN_JOINT )
         .value( "XTION_TILT_JOINT", Joint::XTION_TILT_JOINT )
         .value( "JOINT_NUM", Joint::JOINT_NUM )
-        .export_values();
+        .export_values( );
     
-    py::class_<SobitTurtlebotController>(m, "SobitTurtlebotController")
+    py::class_<SobitTurtlebotController>( m, "SobitTurtlebotController" )
         .def( py::init< const std::string& >() )
         .def( "controlWheelLinear", &SobitTurtlebotController::controlWheelLinear, "control Wheel Linear" )
         .def( "controlWheelRotateRad", &SobitTurtlebotController::controlWheelRotateRad, "control Wheel Rotate Rad" )
         .def( "controlWheelRotateDeg", &SobitTurtlebotController::controlWheelRotateDeg, "control Wheel Rotate Deg" );
     
-    py::class_<SobitEducationController, SobitTurtlebotController>(m, "SobitEducationController")
+    py::class_<SobitEducationController, SobitTurtlebotController>( m, "SobitEducationController" )
         .def( py::init< const std::string& >() )
+        .def( "moveToPose", &SobitEducationController::moveToPose, "move Pose",
+            py::arg( "pose_name" ),
+            py::arg( "sec" ) = 5.0 )
         .def( "moveJoint", &SobitEducationController::moveJoint, "moveJoint", 
-            py::arg("joint_num"), py::arg("rad"), py::arg("sec"), py::arg("is_sleep") = true )
-        .def( "moveXtionPanTilt", &SobitEducationController::moveXtionPanTilt, "move Xtion PanTilt", 
-            py::arg("pan_rad"), py::arg("tilt_rad"), py::arg("sec"), py::arg("is_sleep") = true )
-        .def( "moveArm", &SobitEducationController::moveArm, "move Arm" )
-        .def( "movePose", &SobitEducationController::movePose, "move Pose" )
-        .def( "moveGripperToTarget", &SobitEducationController::moveGripperToTarget, "moveGripperToTarget" )
-        .def( "moveGripperToTargetXYZ", &SobitEducationController::moveGripperToTargetXYZ, "moveGripperToTargetXYZ" );     
+            py::arg( "joint_num" ),
+            py::arg( "rad" ),
+            py::arg( "sec" ) = 5.0,
+            py::arg( "is_sleep" ) = true )
+        .def( "moveHeadPanTilt", &SobitEducationController::moveHeadPanTilt, "move Head PanTilt", 
+            py::arg( "pan_rad" ),
+            py::arg( "tilt_rad" ),
+            py::arg( "sec" ) = 5.0,
+            py::arg( "is_sleep" ) = true )
+        .def( "moveArm", &SobitEducationController::moveArm, "move Arm",
+            py::arg( "arm_roll" ),
+            py::arg( "arm_flex" ),
+            py::arg( "elbow_flex" ),
+            py::arg( "wrist_flex" ),
+            py::arg( "hand_motor" ),
+            py::arg( "sec" ) = 5.0,
+            py::arg( "is_sleep" ) = true )
+        .def( "moveGripperToTargetCoord", &SobitEducationController::moveGripperToTargetCoord, "moveGripperToTargetCoord",
+            py::arg( "goal_position_x" ),
+            py::arg( "goal_position_y" ),
+            py::arg( "goal_position_z" ),
+            py::arg( "diff_goal_position_x" ),
+            py::arg( "diff_goal_position_y" ),
+            py::arg( "diff_goal_position_z" ) )
+        .def( "moveGripperToTargetTF", &SobitEducationController::moveGripperToTargetTF, "moveGripperToTargetTF",
+            py::arg( "target_name" ),
+            py::arg( "diff_goal_position_x" ),
+            py::arg( "diff_goal_position_y" ),
+            py::arg( "diff_goal_position_z" ) );
 }
